e3d71d47d26c1da99a2becde04753b5af23eb9be
David Berry <dberry83702@yahoo.com>
2017-07-29 13:17:40 -0500

    Updated comments and added unit tests for custom exceptions

---
+++ src/Framework.Test/ObjectNotFoundExceptionTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Framework.Test
+{
+    public class ObjectNotFoundExceptionTest
+    {
+
+        [Fact]
+        public void ConstructorMessageShouldPopulateExceptionMessage()
+        {
+            var message = "This is a really interesting message";
+            var exception = new ObjectNotFoundException(message);
+
+            Assert.Equal(message, exception.Message);
+        }
+
+    }
+}
---
+++ src/Framework.Test/ResourceAlreadyExistsExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Framework.Test
+{
+    public class ResourceAlreadyExistsExceptionTest
+    {
+        [Fact]
+        public void ConstructorMessageShouldPopulateExceptionMessage()
+        {
+            var message = "This is a really interesting message";
+            var exception = new ResourceAlreadyExistsException(message);
+
+            Assert.Equal(message, exception.Message);
+        }
+
+    }
+}
