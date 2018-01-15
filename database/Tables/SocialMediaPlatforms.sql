ee05290202d66c0616abaa8e7073b9e9da8f860a
David Berry <dberry83702@yahoo.com>
2018-01-14 21:41:10 -0600

    Added AccountNameRegex column - was added to the database but not added to the DB Project.  Caused problem if someone tried to run the API because the API expects it there

--- database/Tables/SocialMediaPlatforms.sql
+++ database/Tables/SocialMediaPlatforms.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE SocialMediaPlatforms
 (
-    PlatformId      INT           NOT NULL,
-	PlatformName    VARCHAR(30)   NOT NULL,
-	UrlTemplate     VARCHAR(100)  NOT NULL,
+    PlatformId        INT           NOT NULL,
+	PlatformName      VARCHAR(30)   NOT NULL,
+	UrlTemplate       VARCHAR(100)  NOT NULL,
+    AccountNameRegex  VARCHAR (50)  NULL,
 	CONSTRAINT PK_SocialMediaPlatforms
 	    PRIMARY KEY (PlatformId)
 );
