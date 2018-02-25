cb62e6cd29cb7e8c914b6bd863de3d765f440c3b
David Berry <dberry83702@yahoo.com>
2018-02-25 17:13:00 -0600

    Expanded regex in Social media platform table to 100 characters to support longer regexes.  Added regexes for all platforms in initial data

--- database/Data/SocialMediaPlatformsData.sql
+++ database/Data/SocialMediaPlatformsData.sql
@@ -1,10 +1,10 @@
 ﻿MERGE INTO SocialMediaPlatforms AS Target
 USING (VALUES
-    (1, 'Facebook',  'https://www.facebook.com/{AccountName}'),
-    (2, 'Twitter',   'https://www.twitter.com/{AccountName}'),
-    (3, 'Instagram', 'https://www.instagram.com/{AccountName}')
+    (1, 'Facebook',  'https://www.facebook.com/{AccountName}', '^[a-z\d.]{5,}$'),
+    (2, 'Twitter',   'https://www.twitter.com/{AccountName}', '^[a-zA-Z0-9_]{1,15}$'),
+    (3, 'Instagram', 'https://www.instagram.com/{AccountName}', '^([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)$')
 )
-AS Source (PlatformId, PlatformName, UrlTemplate)
+AS Source (PlatformId, PlatformName, UrlTemplate, AccountNameRegex)
 ON Target.PlatformId = Source.PlatformId
     WHEN MATCHED THEN
         UPDATE
@@ -14,4 +14,8 @@ ON Target.PlatformId = Source.PlatformId
     WHEN NOT MATCHED BY TARGET THEN
         INSERT (PlatformId, PlatformName, UrlTemplate)
         VALUES (PlatformId, PlatformName, UrlTemplate);
+
+
+
+
 
--- database/Tables/SocialMediaPlatforms.sql
+++ database/Tables/SocialMediaPlatforms.sql
@@ -3,7 +3,7 @@
     PlatformId        INT           NOT NULL,
 	PlatformName      VARCHAR(30)   NOT NULL,
 	UrlTemplate       VARCHAR(100)  NOT NULL,
-    AccountNameRegex  VARCHAR (50)  NULL,
+    AccountNameRegex  VARCHAR(100)  NULL,
 	CONSTRAINT PK_SocialMediaPlatforms
 	    PRIMARY KEY (PlatformId)
 );
