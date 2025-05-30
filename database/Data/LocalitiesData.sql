5f709972c3fc3211d87d4317a02d4a9d658970fb
David Berry <dberry83702@yahoo.com>
2025-05-29 21:23:58 -0500

    Added sql files to create and populate Localities table

---
+++ database/Data/LocalitiesData.sql
@@ -0,0 +1,17 @@
+﻿MERGE INTO Localities AS Target
+USING (VALUES
+    ('CHI', 'Chicago'),
+    ('RCKFD', 'Rockford'),
+    ('MKE', 'Milwaukee'),
+    ('MAD', 'Madison'),
+    ('NEWI', 'Northeast Wisconsin')
+)
+AS Source (LocalityCode, LocalityName)
+ON Target.LocalityCode = Source.LocalityCode
+    WHEN MATCHED THEN
+        UPDATE
+		    SET
+			LocalityName = Source.LocalityName
+    WHEN NOT MATCHED BY TARGET THEN
+        INSERT (LocalityCode, LocalityName)
+        VALUES (LocalityCode, LocalityName);
---
+++ database/Tables/Localities.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Localities]
+(
+	LocalityCode  VARCHAR(5)      NOT NULL,
+	LocalityName  VARCHAR(30)     NOT NULL,
+	CONSTRAINT PK_Localities
+	    PRIMARY KEY (LocalityCode)
+)
